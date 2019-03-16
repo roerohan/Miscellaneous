5e1150b904a321e7d99459841d4deac3cebf61f6
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-03-17 04:27:52 +0530

    feat: Machine Learning algorithm and implementation based on Minimum Spanning Trees

---
+++ mstModel.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+# matplotlib 1.4 + numpy 1.10 produces warnings; we'll filter these
+import warnings; warnings.filterwarnings('ignore', message='elementwise')
+
+def plot_mst(model, cmap='rainbow'):
+    """Utility code to visualize a minimum spanning tree"""
+    X = model.X_fit_
+    fig, ax = plt.subplots(1, 2, figsize=(16, 6), sharex=True, sharey=True)
+    for axi, full_graph, colors in zip(ax, [True, False], ['lightblue', model.labels_]):
+        segments = model.get_graph_segments(full_graph=full_graph)
+        axi.plot(segments[0], segments[1], '-k', zorder=1, lw=1)
+        axi.scatter(X[:, 0], X[:, 1], c=colors, cmap=cmap, zorder=2)
+        axi.axis('tight')
+
+    ax[0].set_title('Full Minimum Spanning Tree', size=16)
+    ax[1].set_title('Trimmed Minimum Spanning Tree', size=16)
+from sklearn.datasets import make_blobs
+X, y = make_blobs(200, centers=4, random_state=42)
+plt.scatter(X[:, 0], X[:, 1], c='lightblue')
+plt.show()
+
+from mst_clustering import MSTClustering
+model = MSTClustering(cutoff_scale=2, approximate=False)
+labels = model.fit_predict(X)
+plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='rainbow')
+plt.show()
+
+plot_mst(model)
+plt.show()
+
+
+rng = np.random.RandomState(int(100 * y[-1]))
+noise = -14 + 28 * rng.rand(200, 2)
+
+X_noisy = np.vstack([X, noise])
+y_noisy = np.concatenate([y, np.full(200, -1, dtype=int)])
+
+plt.scatter(X_noisy[:, 0], X_noisy[:, 1], c='lightblue', cmap='spectral_r')
+plt.xlim(-15, 15)
+plt.ylim(-15, 15)
+
+plt.show()
+
+noisy_model = MSTClustering(cutoff_scale=1)
+noisy_model.fit(X_noisy)
+plot_mst(noisy_model)
+plt.show()
+
+noisy_model = MSTClustering(cutoff_scale=1, min_cluster_size=10)
+noisy_model.fit(X_noisy)
+plot_mst(noisy_model)
+plt.show()
