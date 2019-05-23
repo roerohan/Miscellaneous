b2c0ff54a1365c9fa8c47b43cd6604878119a820
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-05-24 01:04:55 +0530

    feat: Sample javascript callbacks

---
+++ sample2.js
@@ -0,0 +1,50 @@
+// Sample javascript program, callbacks.
+
+// Comment out all other sections before executing one section.
+
+// Section One
+function slowFunction(num) {
+    setTimeout( () => { // This is a callback function(A function passed inside another)
+        console.log(`You entered ${num}.`);
+        console.log("This took 2 seconds");
+    }, 2000);
+}
+
+slowFunction(10);
+console.log("This gets printed first."); // This line gets printed first, even though it is typed later.
+//This can be made to function in order with callbacks.
+
+// Section Two
+
+// Creating a function with a callback function. The callback is a function, like any other function.
+// In JS you can pass a function as a parameter.
+function slowerFunc(num, callback) {
+    console.log("Waiting for 2.5 seconds.");
+    setTimeout( () => {
+        console.log(`You entered ${num}`);
+        callback(); // Execute the function which was passed as callback.
+    }, 2500);
+}
+
+slowerFunc(15, function () { // Passing a nameless function, alternative: slowerFunc(10, () => {
+    console.log("This gets printed afterwards because this function passed as a callback.")
+});
+
+// Section Three
+
+// You can pass any function as a callback
+
+function someFunc(num, callback) {
+    console.log("Waiting for 3 seconds.");
+    setTimeout( function () { // Passing a nameless function [ same as () => { ]
+        console.log(`You entered ${num}`);
+        callback(); // Execute the function which was passed as callback.
+    }, 3000);
+}
+
+function passAsCallback() {
+    var a = "callback";
+    console.log(`This is the ${a} function`);
+}
+
+someFunc(20, passAsCallback);
