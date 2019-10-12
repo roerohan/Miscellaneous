d3aaff784c92c96acf9acd8e904af1b8797df8c1
fonkamloic <fonkamloic@gmail.com>
2019-10-12 05:18:20 +0200

    Adding a delete item functionality with confirmation
    Code layout restructured and adding a delete item based on age functionality. Taking into account that several employees can have the same age. So deletion is after confirmation indicating employee name and address to delete.

--- LinkedListFunctionality.c
+++ LinkedListFunctionality.c
@@ -19,7 +19,86 @@
     struct Node* next;
 }Node;
 
+//Prototypes
 
+bool push( Node**, int , char [], char [], int);
+bool append( Node** , int , char [], char [], int );
+int input(int* , int* , char [], char []);
+int search(Node* , int , int );
+bool reverse(Node** );
+void printList(Node* ); 
+bool delList(Node **);
+int sampleLinkedList();
+bool delete_node(Node **, int , Node **);
+
+
+
+
+int main() {
+    /* Start with the empty list */
+    Node* head = NULL;
+    int choice = 0;
+    int age = 0, yearsOfExperience = 0;
+    char name[50];
+    char address[100];
+    int key = 0;
+    do{
+        printf("\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", "1. Create a linked list", "2. Insert an item at the beginning",
+               "3. Insert an item at the end", "4. Search an item based on age","5. Delete an item from list", "6. Reverse the list", "7. Print the list", "0. Exit");
+        scanf("%d", &choice);
+        switch(choice){
+            case 1: sampleLinkedList();
+                break;
+
+            case 2: input(&age, &yearsOfExperience, name, address);
+                push(&head, age, name, address, yearsOfExperience);
+                break;
+
+            case 3: input(&age, &yearsOfExperience, name, address);
+                append(&head, age, name, address, yearsOfExperience);
+                break;
+
+            case 4:
+                printf("\nEnter the age to be searched: ");
+                scanf("%d", &key);
+                int index = search(head, key, 0);
+                if(index == -1){
+                    printf("Element not found");
+                }
+                else{
+                    printf("Element found at position %d", index + 1);
+                }
+                break;
+
+
+            case 5:
+                printf("\nEnter the age to be deleted: ");
+                scanf("%d", &key);
+                if( delete_node(&head, key, NULL) == true){
+                    printf("Item deleted sucessfully :)\n");
+                }else
+                    printf("Item not not found or not deleted :(\n");
+                break;
+
+            case 6: reverse(&head);
+                printList(head);
+                break;
+
+            case 7: printList(head);
+                break;
+
+            case 0:  delList(&head);
+                exit(EXIT_SUCCESS);
+
+            default: printf("You've entered a wrong number\n");
+
+        }
+
+    }while(choice!=0);
+    return 0;
+}
+
+ 
 /* Given a reference (pointer to pointer) to the head of a list
 and an int, inserts a new node on the front of the list. */
 bool push( Node** head_ref, int new_age, char name[NAMESIZE], char address[ADDRESSSIZE], int yearsOfExperience) {
@@ -33,7 +112,7 @@ bool push( Node** head_ref, int new_age, char name[NAMESIZE], char address[ADDRE
     new_node -> yearsOfExperience = yearsOfExperience;
 
     //preventing buffer overflow using strncpy
-    strncpy(new_node -> name, name, sizeof(char) * NAMESIZE);  
+    strncpy(new_node -> name, name, sizeof(char) * NAMESIZE);
     strncpy(new_node -> address, address, sizeof(char) * ADDRESSSIZE);
 
     /* 3. Make next of new node as head */
@@ -168,62 +247,48 @@ int sampleLinkedList(){
 }
 
 
+bool delete_node(Node **head, int age , Node **previous){
+  // Base case
+    if (*head == NULL)
+        return false;
+
+    Node *next_node = (*head) ->next, *temp;
+    // If key is present in current node, delete current node and return true
+    if ((*head) -> age == age){
+	    //ack confirmation for deletion, since there can exist several with the same age.
+	    char response[3];
+	    printf("WARMING: Do you really want delete employee with name: %s living in %s [No/yes]?", (*head) ->name, (*head) ->address);
+	     fflush(stdout);
+	     scanf("%s", &response);
+	    if(response[0] == 'y' || response[0] == 'Y'){
+		//deleting happening here.
+
+		    if(previous != NULL && *previous != NULL) {
+
+                (*previous)->next = next_node;
+            } else {
+                previous = head; //just to reference previous to non-null value
+                *previous = next_node;
+            }
+		    //delete node
+		    free(*head);
+            
+		    //update head 
+		    head = &(*previous);
+
+		    return true;
+	    }
+    }
+
+    // Recur for remaining list
+    return delete_node(&((*head) -> next), age, &(*head));
+
+
+}
+
+
 
 int input(int* age, int* yearsOfExperience, char name[], char address[]){
     printf("Enter the following fields respectively: 1) Name, 2) Address, 3) Age, 4) Years of Experience:\n");
     scanf("%s%s%d%d", name, address, age, yearsOfExperience);
-}
-
-int main() {
-    /* Start with the empty list */
-    Node* head = NULL;
-    int choice = 0;
-    int age = 0, yearsOfExperience = 0;
-    char name[50];
-    char address[100];
-    int key = 0;
-    do{
-    printf("\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n", "1. Create a linked list", "2. Insert an item at the beginning",
-    "3. Insert an item at the end", "4. Search an item based on age", "5. Reverse the list", "6. Print the list", "0. Exit");
-    scanf("%d", &choice);
-    switch(choice){
-        case 1: sampleLinkedList();
-        break;
-
-        case 2: input(&age, &yearsOfExperience, name, address);
-        push(&head, age, name, address, yearsOfExperience);
-        break;
-
-        case 3: input(&age, &yearsOfExperience, name, address);
-        append(&head, age, name, address, yearsOfExperience);
-        break;
-
-        case 4:
-        printf("\nEnter the age to be searched: ");
-        scanf("%d", &key);
-        int index = search(head, key, 0);
-        if(index == -1){
-            printf("Element not found");
-        }
-        else{
-            printf("Element found at position %d", index + 1);
-        }
-        break;
-
-        case 5: reverse(&head);
-        printList(head);
-        break;
-
-        case 6: printList(head);
-        break;
-
-        case 0:  delList(&head);
-            exit(EXIT_SUCCESS);
-
-        default: printf("You've entered a wrong number\n");
-
-    }
-
-    }while(choice!=0);
-    return 0;
 }
