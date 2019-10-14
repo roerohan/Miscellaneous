8c90492753bf01379b765e71a49d66d617629fcf
Rohan Mukherjee <rohanbayern@gmail.com>
2019-10-14 10:55:57 +0530

    Update Fibonacci.java

--- Java/Fibonacci.java
+++ Java/Fibonacci.java
@@ -2,19 +2,23 @@ import java.util.*;
 
 class Fibonacci {
 
-static int fib(int n) {
-double phi = (1 + Math.sqrt(5)) / 2;
-return (int) Math.round(Math.pow(phi, n)
-						/ Math.sqrt(5));
-}
-
-public static void main(String[] args) {
-	Scanner sn=new Scanner(System.in);
-	System.out.println("Enter limit:");
-	int n=sn.nextInt();
-	for(int i=1;i<=n;i++)
-	{
-		System.out.println(fib(i));
+	static int fib(int n) {
+		
+		double phi = (1 + Math.sqrt(5)) / 2;
+		return (int) Math.round(Math.pow(phi, n) / Math.sqrt(5));
+		
 	}
-}
+
+	public static void main(String[] args) {
+		
+		Scanner sn = new Scanner(System.in);
+		
+		System.out.println("Enter limit:");
+		int n = sn.nextInt();
+		for(int i = 1; i <= n; i++)
+		{
+			System.out.println(fib(i));
+		}
+		
+	}
 }
