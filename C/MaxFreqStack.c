3e1a78a5436d0cca44a7e2c5ec729663a1b0b6ae
Rohan Mukherjee <rohanbayern@gmail.com>
2020-01-14 19:29:05 +0530

    feat: Added comment lines

--- C/MaxFreqStack.c
+++ C/MaxFreqStack.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
+// Function to create a stack with elements provided by the user
 void createStack(int* arr, int len) {
-    printf("Enter the elements in the stack\n");
+    printf("Enter the elements in the stack:\n");
     for (int i = 0; i < len; i++) {
         scanf("%d", &arr[i]);
     }
 }
 
+// Function to display the current status of the stack
 void display(int* arr, int len) {
-    printf("\nThe current stack is: \n");
+    printf("\nThe current stack is:\n");
     for (int i = 0; i < len; i++) {
         printf("%d\n", arr[i]);
     }
     printf("\n");
 }
 
+// Function to find the element with maximum frequency in the stack
 int findMaxFreqElement(int* arr, int len) {
     int count = 1, tempCount, tempElem;
     int elem = arr[0];
@@ -38,19 +41,21 @@ int findMaxFreqElement(int* arr, int len) {
     return elem;
 }
 
+// Function to pop the element at the index 'index'
 void pop (int* arr, int len, int index) {
     for (int i = index; i < len - 1; i++) {
         arr[i] = arr[i + 1];
     }
 }
 
+// Function to pop the element having maximum frequency
 int popMaxFreq(int* arr, int* len) {
     int elem = findMaxFreqElement(arr, *len);
     for (int i = 0; i < *len; i ++) {
         if (elem == arr[i]) {
             pop(arr, *len, i);
             i--;
-            (*len)--;
+            (*len)--; // Reduce the length of the array everytime an element is popped
         }
     }
     return elem;
@@ -58,7 +63,7 @@ int popMaxFreq(int* arr, int* len) {
 
 int main() {
     int len;
-    printf("Enter the length of the stack\n");
+    printf("Enter the length of the stack:\n");
     scanf("%d", &len);
 
     int arr[len];
