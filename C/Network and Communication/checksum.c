bce0aeec1e3ffe77df126726d169dc239ae6daeb
Rohan Mukherjee <rohanbayern@gmail.com>
2020-02-03 10:48:28 +0530

    feat: Updated checksum program

--- C/Network and Communication/checksum.c
+++ C/Network and Communication/checksum.c
@@ -1,112 +1,42 @@
 #include<stdio.h>
-#include<string.h>
- 
-char* checksum(char a[50], char b[50]) {
-    char sum[strlen(a)], complement[50];
-    int i, length;
-    
-    if(strlen(a)==strlen(b)){
-		length = strlen(a);
-		char carry='0';
-        
-		for(i=length-1;i>=0;i--)
-        {
-			if(a[i]=='0' && b[i]=='0' && carry=='0')
-            {
-                sum[i]='0';
-                carry='0';
-            }
-            else if(a[i]=='0' && b[i]=='0' && carry=='1')
-            {
-                sum[i]='1';
-                carry='0';
- 
-            }
-            else if(a[i]=='0' && b[i]=='1' && carry=='0')
-            {
-                sum[i]='1';
-                carry='0';
- 
-            }
-            else if(a[i]=='0' && b[i]=='1' && carry=='1')
-            {
-                sum[i]='0';
-                carry='1';
- 
-            }
-            else if(a[i]=='1' && b[i]=='0' && carry=='0')
-            {
-                sum[i]='1';
-                carry='0';
- 
-            }
-            else if(a[i]=='1' && b[i]=='0' && carry=='1')
-            {
-                sum[i]='0';
-                carry='1';
- 
-            }
-            else if(a[i]=='1' && b[i]=='1' && carry=='0')
-            {
-                sum[i]='0';
-                carry='1';
- 
-            }
-            else if(a[i]=='1' && b[i]=='1' && carry=='1')
-            {
-                sum[i]='1';
-                carry='1';
- 
-            }
-            else
-                break;
-        }
-        
-        printf(sum);
-        return strncat(&carry, sum, 1);
-		
-	}
-	else {
-		printf("\nWrong input strings");
-        return "Error";
-	}
+
+#include<math.h>
+
+int sender(int b[10], int k) {
+    int checksum, sum = 0, i;
+    printf("\n****SENDER****\n");
+
+    for (i = 0; i < k; i++)
+        sum += b[i];
+    printf("SUM IS: %d", sum);
+
+    checksum = ~sum;
+    printf("\nSENDER's CHECKSUM IS:%d", checksum);
+    return checksum;
 }
 
-int main()
-{   
-    char input[50], sum[50], complement[50];
-    int i = 0, k, m;
-
-    printf("Enter the number of packets: ");
-    scanf("%d", &k);
-
-    printf("Enter the size of a packet: ");
-    scanf("%d", &m);
-
-    printf("Enter the entire message: ");
-    scanf("%s", input);
-
-    if (k*m != strlen(input)) {
-        printf("Message size not valid.");
-        return 1;
-    }
-
-    char temp1[m], temp2[m];
-
-    while (i < m) {
-        temp1[i%m] = input[i];
-        temp2[i%m] = input[i+m];
-        i++;
-    }
-
-    // printf("%s%s", input, checksum(temp1, temp2));
-
-    // while (i < m*k) {
-    //     temp1[i%m] = input[i]; 
-    //     if (i%m == 0) {
-    //         strcpy(temp2, checksum(temp1, temp2));
-    //     }
-    // }
-
-    // printf("%s\n", temp2);
+int receiver(int c[10], int k, int scheck) {
+    int checksum, sum = 0, i;
+    printf("\n\n****RECEIVER****\n");
+    for (i = 0; i < k; i++)
+        sum += c[i];
+    printf(" RECEIVER SUM IS:%d", sum);
+    sum = sum + scheck;
+    checksum = ~sum;
+    printf("\nRECEIVER's CHECKSUM IS:%d", checksum);
+    return checksum;
+}
+int main() {
+    int a[10], i, m, scheck, rcheck;
+    printf("\nENTER SIZE OF THE STRING:");
+    scanf("%d", & m);
+    printf("\nENTER THE ELEMENTS OF THE ARRAY:");
+    for (i = 0; i < m; i++)
+        scanf("%d", & a[i]);
+    scheck = sender(a, m);
+    rcheck = receiver(a, m, scheck);
+    if (rcheck == 0)
+        printf("\n\nNO ERROR IN TRANSMISSION\n\n");
+    else
+        printf("\n\nERROR DETECTED");
 }
