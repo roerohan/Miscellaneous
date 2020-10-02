3e44dc153598effda10844e85bbc8383e1ae8667
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-02 13:47:58 +0530

    fix: remove docker container everytime before creating a new one with same name

--- Dockerfiles/sudomy/sudomy.sh
+++ Dockerfiles/sudomy/sudomy.sh
@@ -7,9 +7,12 @@ PWD="$HOME/Software/sudomy"
 
 NEW_PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/sudomy:/usr/lib/sudomy/lib/bin"
 
+docker rm sudomy 2>/dev/null
+
 docker run \
 	-v "$PWD/output:/usr/lib/sudomy/output" \
 	-v "$PWD/sudomy.api:/usr/lib/sudomy/sudomy.api" \
 	-e "PATH=$NEW_PATH" \
 	-it --name sudomy \
 	screetsec/sudomy:v1.1.8 "$@"
+
