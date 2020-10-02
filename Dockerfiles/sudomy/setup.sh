5481ccb98d980cdacdb2bb83be4b8ccd32744e40
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-02 11:49:51 +0530

    fix: Added shebang

--- Dockerfiles/sudomy/setup.sh
+++ Dockerfiles/sudomy/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker pull screetsec/sudomy:v1.1.8
 
 # docker run -v "${PWD}/output:/usr/lib/sudomy/output" -v "${PWD}/sudomy.api:/usr/lib/sudomy/sudomy.api" -it --rm screetsec/sudomy:v1.1.8 [argument]
