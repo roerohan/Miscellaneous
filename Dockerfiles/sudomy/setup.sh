c8a8d1c0653a44b4d7c6017509f3ea9318b2f7f1
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-02 13:22:22 +0530

    feat: Add symlink to /bin/sudomy

--- Dockerfiles/sudomy/setup.sh
+++ Dockerfiles/sudomy/setup.sh
@@ -3,3 +3,6 @@
 docker pull screetsec/sudomy:v1.1.8
 
 # docker run -v "${PWD}/output:/usr/lib/sudomy/output" -v "${PWD}/sudomy.api:/usr/lib/sudomy/sudomy.api" -it --rm screetsec/sudomy:v1.1.8 [argument]
+
+sudo ln -s "$HOME/Software/sudomy/sudomy.sh" "/bin/sudomy"
+
