b952fe5ae31692af0d465ebe6aade09db85368d8
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-02 12:59:56 +0530

    feat: Added sudomy.sh script to run the docker container

---
+++ Dockerfiles/sudomy/sudomy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# USAGE: ./sudomy.sh -d hackerone.com
+# This will use the docker container
+
+PWD="$HOME/Software/sudomy"
+
+NEW_PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/lib/sudomy:/usr/lib/sudomy/lib/bin"
+
+docker run \
+	-v "$PWD/output:/usr/lib/sudomy/output" \
+	-v "$PWD/sudomy.api:/usr/lib/sudomy/sudomy.api" \
+	-e "PATH=$NEW_PATH" \
+	-it --name sudomy \
+	screetsec/sudomy:v1.1.8 "$@"
