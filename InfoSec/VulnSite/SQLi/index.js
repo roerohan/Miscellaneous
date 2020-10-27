6badd18b6dc32f0eb0ee8cbecddd43423114b180
Rohan Mukherjee <rohanbayern@gmail.com>
2020-10-27 18:38:18 +0530

    feat: Added logger

--- InfoSec/VulnSite/SQLi/index.js
+++ InfoSec/VulnSite/SQLi/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mysql from 'mysql';
 import path from 'path';
 import bodyParser from 'body-parser';
+import winston from 'winston';
 import preventInjection from './preventInjection.js';
 
 const connection = mysql.createConnection({
@@ -11,19 +12,37 @@ const connection = mysql.createConnection({
     database: 'webapp',
 });
 
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service', time: +new Date() },
+    transports: [
+        //
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        //
+        new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: './logs/combined.log' }),
+    ],
+});
+
 connection.connect();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
+    logger.info(req.url);
     res.sendFile(path.resolve('.', 'login.html'));
 });
 
 // app.post('/', preventInjection, (req, res) => {
 app.post('/', (req, res) => {
+    logger.info(req.url);
+    logger.info(req.body);
+
     const { email, password } = req.body;
     const query = `SELECT * FROM USERS WHERE email='${email}' AND password='${password}'`;
 
@@ -38,6 +57,7 @@ app.post('/', (req, res) => {
             res.send('<h1>Logged in successfully!</h1>');
         } else {
             res.send('<h1>Login failed!</h1>');
+            logger.error(req.body);
         }
     });
 });
