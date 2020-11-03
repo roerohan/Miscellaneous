2713782bc32ce862e4cc3634bf97b0a9717136a1
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-03 08:23:07 +0530

    lint: Indentation

--- C/Operating Systems/WorstFit.c
+++ C/Operating Systems/WorstFit.c
@@ -1,30 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
 #define max 25
-void main() {
+void main()
+{
     int frag[max], b[max], f[max], i, j, nb, nf, temp;
     static int bf[max], ff[max];
 
     printf("\n\tMemory Management Scheme - Worst Fit");
     printf("\nEnter the number of blocks:");
-    scanf("%d", & nb);
+    scanf("%d", &nb);
     printf("Enter the number of files:");
-    scanf("%d", & nf);
+    scanf("%d", &nf);
     printf("\nEnter the size of the blocks:-\n");
-    for (i = 1; i <= nb; i++) {
+    for (i = 1; i <= nb; i++)
+    {
         printf("Block %d:", i);
-        scanf("%d", & b[i]);
+        scanf("%d", &b[i]);
     }
     printf("Enter the size of the files :-\n");
-    for (i = 1; i <= nf; i++) {
+    for (i = 1; i <= nf; i++)
+    {
         printf("File %d:", i);
-        scanf("%d", & f[i]);
+        scanf("%d", &f[i]);
     }
-    for (i = 1; i <= nf; i++) {
-        for (j = 1; j <= nb; j++) {
-            if (bf[j] != 1) {
+    for (i = 1; i <= nf; i++)
+    {
+        for (j = 1; j <= nb; j++)
+        {
+            if (bf[j] != 1)
+            {
                 temp = b[j] - f[i];
-                if (temp >= 0) {
+                if (temp >= 0)
+                {
                     ff[i] = j;
                     break;
                 }
