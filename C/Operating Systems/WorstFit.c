e6eeccb3d025ee014cd2eff3e0ddc8c1be5fadbc
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-03 09:12:46 +0530

    feat: added comments for WorstFit

--- C/Operating Systems/WorstFit.c
+++ C/Operating Systems/WorstFit.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 
-#define max 25
+#define max 30
 void main()
 {
     int frag[max], b[max], f[max], i, j, nb, nf, temp;
     static int bf[max], ff[max];
 
-    printf("\n\tMemory Management Scheme - Worst Fit");
+    printf("\n\tMemory Management Scheme - Worst Fit\n");
+
+    // Fill the number of blocks
     printf("\nEnter the number of blocks:");
     scanf("%d", &nb);
+
+    // Fill the number of files
     printf("Enter the number of files:");
     scanf("%d", &nf);
+
+    // Fill the size of the blocks
     printf("\nEnter the size of the blocks:-\n");
     for (i = 1; i <= nb; i++)
     {
         printf("Block %d:", i);
         scanf("%d", &b[i]);
     }
+
+    // Fill the size of the files
     printf("Enter the size of the files :-\n");
     for (i = 1; i <= nf; i++)
     {
         printf("File %d:", i);
         scanf("%d", &f[i]);
     }
+
+    // Memory allocation to the files
     for (i = 1; i <= nf; i++)
     {
         for (j = 1; j <= nb; j++)
@@ -40,7 +50,10 @@ void main()
         frag[i] = temp;
         bf[ff[i]] = 1;
     }
+
+    // Printing result after memory allocation
     printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
+
     for (i = 1; i <= nf; i++)
         printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, f[i], ff[i], b[ff[i]], frag[i]);
 }
