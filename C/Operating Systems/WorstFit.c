d7d09547c3209d23db66e9b0669e8cd9f9f0a981
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-03 09:18:53 +0530

    fix: Worst fit algo

--- C/Operating Systems/WorstFit.c
+++ C/Operating Systems/WorstFit.c
@@ -1,59 +1,57 @@
 #include <stdio.h>
 
-#define max 30
-void main()
+#define size 30
+
+int main()
 {
-    int frag[max], b[max], f[max], i, j, nb, nf, temp;
-    static int bf[max], ff[max];
+    printf("\nWorst Fit\n");
+    int frag[size], b[size], f[size];
+    int i, j, nb, nf, var, highest = 0;
+    int bf[size], ff[size];
 
-    printf("\n\tMemory Management Scheme - Worst Fit\n");
-
-    // Fill the number of blocks
-    printf("\nEnter the number of blocks:");
+    printf("\nEnter the number of blocks: ");
     scanf("%d", &nb);
-
-    // Fill the number of files
-    printf("Enter the number of files:");
+    printf("Enter the number of processes: ");
     scanf("%d", &nf);
-
-    // Fill the size of the blocks
     printf("\nEnter the size of the blocks:-\n");
+
     for (i = 1; i <= nb; i++)
     {
-        printf("Block %d:", i);
+        printf("Block %d: ", i);
         scanf("%d", &b[i]);
     }
 
-    // Fill the size of the files
-    printf("Enter the size of the files :-\n");
+    printf("Enter the size of the processes:-\n");
+
     for (i = 1; i <= nf; i++)
     {
-        printf("File %d:", i);
+        printf("Process %d: ", i);
         scanf("%d", &f[i]);
     }
-
-    // Memory allocation to the files
     for (i = 1; i <= nf; i++)
     {
         for (j = 1; j <= nb; j++)
         {
             if (bf[j] != 1)
             {
-                temp = b[j] - f[i];
-                if (temp >= 0)
-                {
-                    ff[i] = j;
-                    break;
-                }
+                var = b[j] - f[i];
+                if (var >= 0)
+                    if (highest < var)
+                    {
+
+                        ff[i] = j;
+                        highest = var;
+                    }
             }
         }
-        frag[i] = temp;
+
+        frag[i] = highest;
         bf[ff[i]] = 1;
+        highest = 0;
     }
 
-    // Printing result after memory allocation
-    printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
-
+    printf("\nProcess_no\tProcess_size\tBlock_no\tBlock_size\tFragment");
     for (i = 1; i <= nf; i++)
         printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, f[i], ff[i], b[ff[i]], frag[i]);
+    printf("\n");
 }
