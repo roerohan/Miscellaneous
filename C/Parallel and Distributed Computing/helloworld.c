f90793da5f2633a7fb6d9b7b66b0d618d8ccc8b1
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-06 17:45:19 +0530

    lint: Helloworld.c

--- C/Parallel and Distributed Computing/helloworld.c
+++ C/Parallel and Distributed Computing/helloworld.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <omp.h>
 #define N 5
-int main(void) { 
+int main(void)
+{
     int i;
     int nProc = omp_get_max_threads();
     printf("Number of processors available: %d\n", omp_get_num_procs());
@@ -10,12 +11,13 @@ int main(void) {
 
     printf("Dynamic adjustment of threads enabled: %s\n", omp_get_dynamic() ? "true" : "false");
     printf("Nested parallelism enabled: %s\n", omp_get_nested() ? "true" : "false");
-    #pragma omp parallel private(i)
-    {    
+#pragma omp parallel private(i)
+    {
         printf("Number of threads: %d\n", omp_get_num_threads());
-	printf("Current thread ID: %d\n", omp_get_thread_num());
-        #pragma omp for             
-        for (i = 0; i < N; i++) {
+        printf("Current thread ID: %d\n", omp_get_thread_num());
+#pragma omp for
+        for (i = 0; i < N; i++)
+        {
             puts("Hello World!");
         }
     }
